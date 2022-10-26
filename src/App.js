c9696721e2f8fe200e4a2b2ccfcc129cb67718ef
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-10-26 16:15:05 +0900

    202210261614

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return;
+  <div className="App">
+    <Home />
+    <Page1 />
+    <Page2 />
+  </div>;
 }
