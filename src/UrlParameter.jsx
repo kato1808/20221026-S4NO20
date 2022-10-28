d19b49907abc949c9fa3351d04789939037a0207
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-10-28 17:09:52 +0900

    202210281709

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  // const location = useLocation();
+  // console.log(location);
   return (
     <div>
       <h1>UrlParameterページです</h1>
